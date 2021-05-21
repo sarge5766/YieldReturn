24e75835bde1108443f75eeff208a6ae05c6f95d
Scott Lampron <scott.lampron@gmail.com>
2021-05-21 05:44:02 -0400

    Added Yield and NoYield methods

---
+++ YieldReturn/Person.cs
@@ -0,0 +1,7 @@
+﻿namespace YieldReturn {
+    class Person {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+    }
+}
--- YieldReturn/Program.cs
+++ YieldReturn/Program.cs
@@ -1,11 +1,52 @@
 ﻿using System;
+using System.Collections;
 
 namespace YieldReturn {
     class Program {
         static void Main(string[] args) {
+            // NO YIELD
+            NoYield();
 
+            // YIELD
+            WithYield();
 
-            Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
+
+        #region NO YIELD
+        static void NoYield() {
+            int[] a = new int[10];
+            a = FuncNoYield(2, 10);
+            for (int i = 0; i < 10; i++) {
+                Console.WriteLine(a[i]);
+            }
+        }
+
+        private static int[] FuncNoYield(int start, int number) {
+            int[] _number = new int[number];
+
+            for (int i = 0; i < number; i++) {
+                _number[i] = start + 2 * i;
+            }
+            return _number;
+        }
+        #endregion
+
+        #region WITH YIELD
+        static void WithYield() {
+            foreach (var item in FuncWithYield(2, 10)) {
+                var temp = item;
+
+                Console.WriteLine(item);
+            }
+        }
+
+        private static IEnumerable FuncWithYield(int start, int number) {
+            for (int i = 0; i < number; i++) {
+                yield return start + 2 * i;
+            }
+        }
+        #endregion
+
     }
 }
